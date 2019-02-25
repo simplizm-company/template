c8da964cb20b0769af61de43296db875005d2cbc
simplizm-company <40747170+simplizm-company@users.noreply.github.com>
2019-02-25 17:45:14 +0900

    eclipse

--- project/src/assets/js/plugin/eclipse.js
+++ project/src/assets/js/plugin/eclipse.js
@@ -26,7 +26,10 @@
                 arrow: true, // arrow navigation 사용 여부
                 pager: true, // pager 사용 여부
                 slidesToShow: 1, // 한번에 보여질 slide 갯수
-                speed: 500 // 전환 속도
+                speed: 500, // 전환 속도
+                view: 1,
+                move: 1,
+                margin: 0
             }
 
             _.options = $.extend({}, _.defaults, settings);
@@ -36,7 +39,10 @@
                 thisIndex: 0, // 현재 index
                 sliderWidth: 0, // 슬라이더 너비값
                 nextIndex: 0, // 다음 index
-                transition: _.options.speed + 'ms'
+                transition: _.options.speed + 'ms', // transition 속도
+                playActionFlag: false, // 시작종료 flag
+                viewIndex: [], // 보여지는 slides의 배열
+                computedIndexArray: [] // 계산용 index 배열
             }
 
             _.$eclipse = $(element);
@@ -51,64 +57,98 @@
         // 예비동작
         var _ = this;
 
-        _.$slides.each(function (i) {
-            if (prevOrNext === 'next') {
-                this.range = i - thisIndex < 0 ? i - thisIndex + _.initials.slideCount : i - thisIndex;
-            }
-            if (prevOrNext === 'prev') {
-                this.range = i - thisIndex > 0 ? i - thisIndex - _.initials.slideCount : i - thisIndex;
-            }
-            this.point = this.range;
-            this.left = this.width * this.point;
-            this.transform = 'translate3d(' + this.left + 'px, 0, 0)';
-            $(this).stop().css(_.autoPrefixer(0, 'none', this.transform));
-        }).promise().done(function () {
-            setTimeout(function () {
-                callback();
-            }, 10);
-        });
+        if (!_.initials.playActionFlag) {
+            _.initials.playActionFlag = true;
+            _.$slides.each(function (i) {
+                this.isView = undefined;
+                // console.log(_.initials.viewIndex);
+                for (var j = 0; j < _.initials.viewIndex.length; j++) {
+                    if (this.index === _.initials.viewIndex[j]) {
+                        this.isView = _.initials.viewIndex.indexOf(_.initials.viewIndex[j]);
+                    }
+                }
+                this.point = this.index - _.initials.thisIndex;
+                if (prevOrNext === 'next') {
+                    this.point = i - thisIndex < 0 ? i - thisIndex + _.initials.slideCount : i - thisIndex;
+                }
+                if (prevOrNext === 'prev') {
+                    if (this.isView !== undefined) {
+                        this.point = this.isView;
+                    } else {
+                        this.point = i - thisIndex > 0 ? i - thisIndex - _.initials.slideCount : i - thisIndex;
+                    }
+                }
+                this.left = this.width * this.point + (_.options.margin * this.point);
+                this.transform = 'translate3d(' + this.left + 'px, 0, 0)';
+                $(this).stop().css(_.autoPrefixer(0, 'none', this.transform));
+            }).promise().done(function () {
+                setTimeout(function () {
+                    callback();
+                }, 50);
+            });
+        }
     }
 
     Eclipse.prototype.goToSlidesPrevOrNext = function (nextIndex) {
         var _ = this;
 
-        _.initials.thisIndex = _.initials.thisIndex + nextIndex;
+        var computedNextIndex = nextIndex;
+
+        if (nextIndex > 0 && _.initials.thisIndex + (_.options.slidesToShow - 1) !== _.initials.slideCount - 1) {
+            computedNextIndex = _.initials.thisIndex + nextIndex + (_.options.slidesToShow - 1) > _.initials.slideCount - 1 ?
+            (_.initials.slideCount - 1) - (_.options.slidesToShow - 1) - _.initials.thisIndex :
+            nextIndex;
+        }
+        if (nextIndex < 0 && _.initials.thisIndex !== 0) {
+            computedNextIndex = _.initials.thisIndex + nextIndex < 0 ?
+            0 - _.initials.thisIndex :
+            nextIndex;
+        }
 
         _.$slides.each(function (i) {
-            this.point -= nextIndex;
-            this.left = this.width * this.point;
+            this.point -= computedNextIndex;
+            this.left = this.width * this.point + (_.options.margin * this.point);
             this.transform = 'translate3d(' + this.left + 'px, 0, 0)';
-            if (_.initials.thisIndex === this.point) {
-                $(this).addClass('eclipse-active').siblings().removeClass('eclipse-active');
-            }
             $(this).stop().css(_.autoPrefixer(0, _.initials.transition, this.transform));
         }).promise().done(function () {
-            _.initials.thisIndex = _.computedIndex(_.initials.thisIndex);
+            _.initials.thisIndex = _.initials.computedIndexArray[_.initials.thisIndex + _.initials.slideCount + computedNextIndex];
+            _.setSlideEnd(computedNextIndex);
         });
     }
 
     Eclipse.prototype.goToSlides = function (nextIndex) {
         var _ = this;
 
-        var movetype = _.initials.thisIndex !== nextIndex ? _.initials.thisIndex < nextIndex ? 'next' : 'prev' : false;
-
-        if (movetype) {
-            _.preparationAction(_.initials.thisIndex, movetype, function () {
+        if (_.initials.thisIndex !== nextIndex && !_.initials.playActionFlag) {
+            var computedNextIndex = nextIndex + (_.options.slidesToShow - 1) > _.initials.slideCount - 1 ?
+            (_.initials.slideCount - 1) - (_.options.slidesToShow - 1) :
+            nextIndex;
+            _.preparationAction(_.initials.thisIndex, false, function () {
                 _.$slides.each(function (i) {
-                    this.point -= nextIndex - _.initials.thisIndex;
-                    this.left = this.width * this.point;
+                    this.point -= computedNextIndex - _.initials.thisIndex;
+                    this.left = this.width * this.point + (_.options.margin * this.point);
                     this.transform = 'translate3d(' + this.left + 'px, 0, 0)';
-                    if (nextIndex === this.index) {
-                        $(this).addClass('eclipse-active').siblings().removeClass('eclipse-active');
-                    }
                     $(this).stop().css(_.autoPrefixer(0, _.initials.transition, this.transform));
                 }).promise().done(function () {
-                    _.initials.thisIndex = nextIndex;
+                    _.setSlideEnd(computedNextIndex - _.initials.thisIndex);
+                    _.initials.thisIndex = computedNextIndex;
                 });
             });
-        } else {
-            return;
         }
+    }
+
+    Eclipse.prototype.setSlideEnd = function (nextIndex) {
+        var _ = this;
+
+        _.$slides.removeClass('eclipse-active');
+        for (var j = 0; j < _.initials.viewIndex.length; j++) {
+            _.initials.viewIndex[j] = _.initials.computedIndexArray[_.initials.viewIndex[j] + _.initials.slideCount + nextIndex];
+            _.$slides.eq(_.initials.viewIndex[j]).addClass('eclipse-active');
+        }
+        console.log(_.initials.viewIndex);
+        setTimeout(function () {
+            _.initials.playActionFlag = false;
+        }, _.options.speed);
     }
 
     Eclipse.prototype.buildControls = function () {
@@ -145,12 +185,6 @@
         }
     }
 
-    Eclipse.prototype.computedIndex = function (value) {
-        var _ = this;
-
-        return value < _.initials.slideCount ? value > -1 ? value : _.initials.slideCount - 1 : 0;
-    }
-
     Eclipse.prototype.setSlidesCSS = function () {
         var _ = this;
 
@@ -163,23 +197,35 @@
 
     Eclipse.prototype.setSlidesEach = function () {
         var _ = this;
+        var indexTemp = [];
+        var vi = _.options.startIndex;
 
         _.$slides.each(function (i) {
-            this.width = _.initials.sliderWidth;
+            this.width = (_.initials.sliderWidth - (_.options.slidesToShow - 1) * _.options.margin) / _.options.slidesToShow;
             this.height = $(this).height();
             this.index = i;
-            this.left = this.width * this.index;
+            this.point = i - _.options.startIndex < 0 ? i - _.options.startIndex + _.initials.slideCount : i - _.options.startIndex;
+            this.left = this.index == _.options.startIndex ? this.width * this.point : (this.width * this.point) + (this.point * _.options.margin);
+            this.transform = 'translate3d(' + this.left + 'px, 0, 0)';
 
-            $(this).css({
-                'width': this.width,
-                'left': this.left
-            });
+            indexTemp.push(i);
+
+            $(this).css({'width': this.width}).css(_.autoPrefixer(0, 'none', this.transform));
         }).promise().done(function () {
+            for (var i = 0; i < 3; i++) {
+                _.initials.computedIndexArray.push.apply(_.initials.computedIndexArray, indexTemp);
+            }
+
             _.$slider.css({
                 'overflow': 'hidden',
                 'position': 'relative',
                 'height': _.$slides[_.initials.thisIndex].height
             });
+
+            for (var i = 0; i < _.options.slidesToShow; i++) {
+                _.initials.viewIndex[i] = vi;
+                vi = vi + 1 > _.initials.slideCount - 1 ? 0 : vi + 1;
+            }
         });
     }
 
@@ -189,6 +235,7 @@
         _.initials.slideCount = _.$slides.length;
         _.initials.thisIndex = _.options.startIndex;
         _.initials.sliderWidth = _.$slider.width();
+        _.options.move = _.initials.slideCount / _.options.slidesToShow < _.options.move ? 1 : _.options.move;
     }
 
     Eclipse.prototype.setEvents = function () {
@@ -196,18 +243,18 @@
 
         _.$arrowPrev.on('click', function () {
             _.preparationAction(_.initials.thisIndex, 'prev', function () {
-                _.goToSlidesPrevOrNext(-1);
+                _.goToSlidesPrevOrNext(-_.options.move);
             });
         });
 
         _.$arrowNext.on('click', function () {
             _.preparationAction(_.initials.thisIndex, 'next', function () {
-                _.goToSlidesPrevOrNext(1);
+                _.goToSlidesPrevOrNext(_.options.move);
             });
         });
 
         _.$pagingButton.on('click', function () {
-            _.goToSlides($(this).index());
+            _.goToSlides($(this).index() * _.options.slidesToShow);
         });
     }
 
