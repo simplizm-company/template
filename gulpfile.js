bd61584ffb4a556e3509f2116dc25960a8614007
simplizm-company <40747170+simplizm-company@users.noreply.github.com>
2019-03-05 18:45:39 +0900

    clear

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,18 @@ var autoprefixer = require('gulp-autoprefixer');
 var fileinclude = require('gulp-file-include');
 var browserSync = require('browser-sync').create();
 
-gulp.task('server', ['fonts', 'images', 'html', 'js', 'sass', 'css'], function () {
+gulp.task('server', ['allfile', 'scss'], function () {
     browserSync.init({
         port: 1234,
         server: {
             baseDir: 'project/dist'
         }
     });
-    gulp.watch('project/src/assets/fonts/**/*.*', ['fonts']);
-    gulp.watch('project/src/assets/images/**/*.*', ['images']);
-    gulp.watch('project/src/**/*.*', ['html']);
-    gulp.watch('project/src/assets/js/**/*.js', ['js']);
-    gulp.watch('project/src/assets/scss/**/*.scss', ['sass']);
-    gulp.watch('project/src/assets/css/**/*.css', ['css']);
+    gulp.watch([
+        'project/src/**/*.*',
+        '!project/src/**/*.scss'
+    ], ['allfile']);
+    gulp.watch('project/src/assets/scss/**/*.scss', ['scss']);
     gulp.watch('project/src/**', function (e) {
         if (e.type === 'deleted') {
             var filePathFromSrc = path.relative(path.resolve('project/src/'), e.path);
@@ -33,36 +32,20 @@ gulp.task('server', ['fonts', 'images', 'html', 'js', 'sass', 'css'], function (
     gulp.watch('project/dist/**/*.*', browserSync.reload);
 });
 
-gulp.task('html', function () {
+gulp.task('allfile', function () {
     return gulp
-        .src('project/src/views/**/*.*')
+        .src([
+            'project/src/**/*.*',
+            '!project/src/**/*.scss'
+        ])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
-        .pipe(gulp.dest('project/dist/views'))
+        .pipe(gulp.dest('project/dist'))
 });
 
-gulp.task('js', function () {
-    return gulp
-        // 모든 js파일을 하나의 파일로 압축할 때 사용
-        // .src([
-        //     'project/src/assets/js/library/jquery-3.2.1.min.js',
-        //     'project/src/assets/js/plugin/cookie.js',
-        //     'project/src/assets/js/plugin/slick.js',
-        //     'project/src/assets/js/plugin/sunrise.js',
-        //     'project/src/assets/js/ui.js'
-        // ])
-        // .pipe(concat('conbined.js'))
-        // .pipe(gulp.dest('project/dist/assets/js'))
-        // .pipe(uglify())
-        // .pipe(rename('combined.min.js'))
-        // .pipe(gulp.dest('project/dist/assets/js'));
-        .src('project/src/assets/js/**/*.js')
-        .pipe(gulp.dest('project/dist/assets/js'));
-});
-
-gulp.task('sass', function () {
+gulp.task('scss', function () {
     return gulp
         .src('project/src/assets/scss/**/*.scss')
         .pipe(sass({
@@ -72,24 +55,6 @@ gulp.task('sass', function () {
             browsers: ['last 2 versions', 'ie >= 9']
         }))
         .pipe(gulp.dest('project/src/assets/css'))
-});
-
-gulp.task('css', function () {
-    return gulp
-        .src('project/src/assets/css/**/*.css')
-        .pipe(gulp.dest('project/dist/assets/css'))
-});
-
-gulp.task('fonts', function () {
-    return gulp
-        .src('project/src/assets/fonts/*.*')
-        .pipe(gulp.dest('project/dist/assets/fonts'))
-});
-
-gulp.task('images', function () {
-    return gulp
-        .src('project/src/assets/images/**/*.*')
-        .pipe(gulp.dest('project/dist/assets/images'))
 });
 
 gulp.task('default', ['server']);
